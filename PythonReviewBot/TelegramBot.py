25cdb95e4750b2cc13f39aef17d4c6593be8d9d6
bulygin artem  konstantinovich <bulygin.ak@phystech.edu>
2019-05-10 17:35:24 +0300

    Слил парсер в бота

--- PythonReviewBot/TelegramBot.py
+++ PythonReviewBot/TelegramBot.py
@@ -1,5 +1,6 @@
 from telegram.ext import MessageHandler, Filters, Updater
-from SearchFunctions import *
+from requests import get
+from bs4 import BeautifulSoup
 
 
 class DialogBot:
@@ -10,8 +11,41 @@ class DialogBot:
         self.updater.dispatcher.add_handler(handler)
         self.handlers = {}
 
-    def run(self):
-        self.updater.start_polling()
+    def link_finder(self, soup_text):
+        if 'No results found.' in str(soup_text):
+            return None
+        else:
+            for h in soup_text.findAll('h1'):
+                link = h.find('a')
+                if link:
+                    founded_link = link['href']
+                    return founded_link
+
+    def information_finder(self, link):
+        if link:
+            new_req = get(link)
+            soup = BeautifulSoup(new_req.text, "lxml")
+            found_article = link.split('/')[4]
+            found_article = found_article.split('_')
+            for paragraph in soup.findAll('p'):
+                check_for_paragraph = 1
+                for word in found_article:
+                    if word not in str(paragraph) and word.lower() not in str(paragraph):
+                        check_for_paragraph = 0
+                        break
+                if '<b>' in str(paragraph) and check_for_paragraph:
+                    ret_text = str(paragraph)[3:len(str(paragraph)) - 4] + \
+                               '\n\n' + 'For more detail information you can go to this site:\n' + link
+                    return ret_text
+
+        else:
+            return 'I\'m sorry, but i can\'t find anything for your request'
+
+    def search(self, request):
+        req = get('https://starwars.fandom.com/wiki/Special:Search?query=' + request)
+        soup = BeautifulSoup(req.text, "lxml")
+        founded_link = self.link_finder(soup)
+        return self.information_finder(founded_link)
 
     def handle_message(self, bot, update):
         chat_id = update.message.chat_id
@@ -35,7 +69,7 @@ class DialogBot:
                 else:
                     bot.sendMessage(chat_id, 'Sorry, I don\'t understand you.')
             elif self.handlers[chat_id] == 'Waiting for request':
-                information = search(message)
+                information = self.search(message)
                 bot.sendMessage(chat_id, information, 'HTML')
                 bot.sendMessage(chat_id,
                                 'What are you want? I understand only <b>\'exit\'</b> and <b>\'find\'</b>',
@@ -51,3 +85,5 @@ class DialogBot:
                             'HTML')
             self.handlers[chat_id] = 'Waiting for command'
 
+    def run(self):
+        self.updater.start_polling()
